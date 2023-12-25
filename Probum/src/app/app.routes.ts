a84ea1838e7963bb63e51bf42c618a5739881fce
Telmo Maciel <telmomaciel9@gmail.com>
2023-12-25 23:18:21 +0000

    Inicio perfil

--- Probum/src/app/app.routes.ts
+++ Probum/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { ProvasComponent } from './Provas/provas.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { PerfilComponent } from './Utilizador/perfil/perfil.component';
+import { EditarPerfilComponent } from './Utilizador/editar-perfil/editar-perfil.component';
 
 export const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'provas', component: ProvasComponent },
+  { path: 'perfil', component: PerfilComponent },
+  { path: 'editar', component: EditarPerfilComponent },
 ];
