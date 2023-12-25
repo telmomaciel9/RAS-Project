7c8d4f66565570d7f931f95deea4277a8063a421
Telmo Maciel <telmomaciel9@gmail.com>
2023-12-25 02:40:37 +0000

    menu

--- Probum/src/app/app.component.ts
+++ Probum/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,MatToolbarModule,MatIconModule],
+  imports: [CommonModule, RouterOutlet,MatToolbarModule,MatIconModule,MatMenuModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
