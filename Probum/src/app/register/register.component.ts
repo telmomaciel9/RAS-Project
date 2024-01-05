4fc51e429d30ba17884a899bc0a3e9fed5c93597
Lucena02 <riku.lucena@hotmail.com>
2024-01-05 22:23:00 +0000

    Register e outros

--- Probum/src/app/register/register.component.ts
+++ Probum/src/app/register/register.component.ts
@@ -9,14 +9,16 @@ import {MatDividerModule} from '@angular/material/divider';
 import { AuthService } from '../homepage/homepage.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [MatFormFieldModule,FormsModule, MatInputModule, MatButtonModule, MatIconModule, MatCardModule, FlexLayoutModule, MatDividerModule, HttpClientModule,],
+  imports: [MatSelectModule, MatFormFieldModule,FormsModule, MatInputModule, MatButtonModule, MatIconModule, MatCardModule, FlexLayoutModule, MatDividerModule, HttpClientModule,],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-
+  hide = true
+  selected = 'option2';
 }
